38cdc44a0eff55e563b2d2eb296de65195a14142
sayannath <sayannath235@gmail.com>
2025-06-02 18:53:08 -0600

    Added the training files for KNN

--- evaluation/german/german_KNN_AOD_sex.py
+++ evaluation/german/german_KNN_AOD_sex.py
@@ -472,3 +472,18 @@ print(disparate_impact(data_orig_test, predictions, "sex"))
 print(statistical_parity_difference(data_orig_test, predictions, "sex"))
 print(equal_opportunity_difference(data_orig_test, predictions, y_test, "sex"))
 print(average_odds_difference(data_orig_test, predictions, y_test, "sex"))
+
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+print("Precision:", precision_score(y_test, predictions))
+print("Recall:", recall_score(y_test, predictions))
+print("F1 score:", f1_score(y_test, predictions))
+
+import json
+from utils.file_ops import write_file
+from utils.run_history import _get_run_history
+
+write_file(
+    "./run_history/german_knn_aod_sex_run_history.json",
+    json.dumps(_get_run_history(automl_model=automl), indent=4),
+)
